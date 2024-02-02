f6041a88e67206cf380f37666fa946d91009071e
mertCukadar <cukadar.mertkaan@gmail.com>
2024-02-02 22:16:38 +0300

    provider added

--- GMACol/App.js
+++ GMACol/App.js
@@ -3,10 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import GreetingScreen from './src/Screens/GreetingScreen';
 import TestScreen from './src/Screens/TestScreen';
+import React from 'react';
+import { AxiosProvider } from './src/Context/AxiosContext';
 
 export default function App() {
+
+
   const Stack = createStackNavigator();
   return (
+    <AxiosProvider>
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
@@ -20,6 +25,7 @@ export default function App() {
         <Stack.Screen name="TestScreen" component={TestScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+    </AxiosProvider>
   );
 }
 
