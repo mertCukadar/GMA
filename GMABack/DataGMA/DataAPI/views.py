906b6aaf4e6fb7853c7700f0e79d07078c714898
mertCukadar <cukadar.mertkaan@gmail.com>
2024-02-02 22:16:04 +0300

    views added

--- GMABack/DataGMA/DataAPI/views.py
+++ GMABack/DataGMA/DataAPI/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class DataAPI(APIView):
+    def post(self, request):
+        data = request.data
+        print(data)
+        return Response({'data': data})
+    
+
+class TestAPI(APIView):
+    def get(self, request):
+        return Response({'data': 'Hello World!'})
