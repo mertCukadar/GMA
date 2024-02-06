431b76ed961535b2496aff7397d9c3965992886e
mertCukadar <cukadar.mertkaan@gmail.com>
2024-02-06 22:33:59 +0300

    Data api fixed

--- GMABack/DataGMA/DataAPI/views.py
+++ GMABack/DataGMA/DataAPI/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import sensorDataSerializer
 # Create your views here.
 
 class DataAPI(APIView):
     def post(self, request):
         data = request.data
-        print(data)
-        return Response({'data': data})
-    
-
-class TestAPI(APIView):
-    def get(self, request):
-        return Response({'data': 'Hello World!'})
+        serializer = sensorDataSerializer(data = data , many = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'data': 'Data saved successfully'})
+        else:
+            return Response(serializer.errors)
