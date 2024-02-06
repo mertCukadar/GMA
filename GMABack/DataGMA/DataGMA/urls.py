8c3ff93311d5ec14452d80053839d2740c04345a
mertCukadar <cukadar.mertkaan@gmail.com>
2024-02-06 22:34:04 +0300

    urls added

--- GMABack/DataGMA/DataGMA/urls.py
+++ GMABack/DataGMA/DataGMA/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from DataAPI.views import DataAPI
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('DataAPI.urls')),   
+    path('api/', include('DataAPI.urls')),
+    path('user/' , include('UserAPI.urls'))
 ]
 
