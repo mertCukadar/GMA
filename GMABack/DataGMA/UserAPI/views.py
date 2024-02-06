efcb2e678f449e4f0321c59d52ff8a84f544b4c7
mertCukadar <cukadar.mertkaan@gmail.com>
2024-02-06 22:35:05 +0300

    user api added

--- GMABack/DataGMA/UserAPI/views.py
+++ GMABack/DataGMA/UserAPI/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from .models import User
 
 # Create your views here.
+class UserAPI(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            user = User.objects.all().last()
+            
+            #take the last user
+            return Response({'id' : user.UserID})
+        else:
+            return Response(serializer.errors)
