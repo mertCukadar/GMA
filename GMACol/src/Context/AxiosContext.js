b013619d4c6beade6a3507064238e25f694e457f
mertCukadar <cukadar.mertkaan@gmail.com>
2024-02-06 22:35:17 +0300

    axios content update

--- GMACol/src/Context/AxiosContext.js
+++ GMACol/src/Context/AxiosContext.js
@@ -1,11 +1,17 @@
-import React, { createContext } from "react";
+import React, { createContext , useState} from "react";
 import axios from "axios";
 
 export const AxiosContext = createContext();
 
+
+
 export const AxiosProvider = ({ children }) => {
+    const [UserId , setUserId] = useState(0);
     const instance = axios.create({
-        baseURL: 'http://192.168.1.103:8000/api/',
+        baseURL: 'http://192.168.1.109:8000/api/',
+    });
+    const userInstance = axios.create({
+        baseURL: 'http://192.168.1.109:8000/user/',
     });
 
     const sendData = async (url, data) => {
@@ -16,9 +22,19 @@ export const AxiosProvider = ({ children }) => {
             console.log(e);
         }
     }
+
+    const sendUserData = async (url, data) => {
+        try {
+            const response = await userInstance.post(url, data);
+            response.data.id ? setUserId(response.data.id) : null;
+            return response;
+        } catch (e) {
+            console.log(e);
+        }
+    }
     
     return (
-        <AxiosContext.Provider value={{ sendData }}>
+        <AxiosContext.Provider value={{ sendData , UserId , sendUserData}}>
             {children}
         </AxiosContext.Provider>
     );
