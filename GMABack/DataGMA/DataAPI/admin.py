de07038e8f090b49eb3c6984b2314af724b710dd
mertCukadar <cukadar.mertkaan@gmail.com>
2024-02-06 22:32:43 +0300

    admin display register added

--- GMABack/DataGMA/DataAPI/admin.py
+++ GMABack/DataGMA/DataAPI/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import textData , textDataUser , sensorData
+from .models import sensorData
 
 
 # Register your models here.
-admin.site.register(textData)
-admin.site.register(textDataUser)
-admin.site.register(sensorData)
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('UserID', 'latter', 'gyroX', 'gyroY', 'gyroZ', 'accelX', 'accelY', 'accelZ')
+    list_filter = ['latter']
+    search_fields = ['latter']
+
+admin.site.register(sensorData, MemberAdmin)
